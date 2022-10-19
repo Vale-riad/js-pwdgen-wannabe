63675a5ed46cdc4d54872ed9ca5825649b8c2f11
Vale-riad <valentinariad91@gmail.com>
2022-10-19 13:31:33 +0200

    Agg: nome, cognome, col preferito dell'utente

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// 1. Chiedo il nome all'untente
+const firstName = prompt("Inserisci il tuo nome");
+// 2.Chiedo il suo cognome
+const lastName = prompt("Inserisci il tuo cognome");
+// 3. Chiedo il suo colore preferito
+const favouriteColor = prompt("Inserisci il tuo colore preferito");
+// 4. Stampare all'interno della pagina la seguente stringa"nomecognomecolorepreferito76"
